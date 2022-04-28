d0dd020358742f81115a90ed20eccac3ee04ef2c
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-28 14:30:45 -0400

    change

--- my-new-app/src/components/redux/reducers/index.js
+++ my-new-app/src/components/redux/reducers/index.js
@@ -3,19 +3,4 @@ import { combineReducers } from "redux";
 import { notesReducer } from "./notes";
 import { userReducer } from "./user";
 
-/**
- * Example state:
- * ```
- * {
- *   notes: {
- *    "fsdafsa1": { id: "fsdafsa1", title: "", description: "", createdAt: 1649466420222 },
- *    "3892472q": { id: "3892472q", title: "", description: "", createdAt: 1649466420223 },
- *   },
- *   user: {
- *    username: "asdf",
- *    loggedIn: true,
- *   },
- * }
- * ```
- */
 export default combineReducers({ notes: notesReducer, user: userReducer });
